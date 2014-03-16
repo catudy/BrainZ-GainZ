cfeff79709e1426146100c728028a907246672bb
Albert L Wohletz <albertlwohletz@Alberts-MacBook-Pro.local>
2014-03-16 01:00:34 -0700

    Updated oscillation script to be customizable for any object.

--- Assets/Scripts/Oscillate.cs
+++ Assets/Scripts/Oscillate.cs
@@ -1,15 +1,24 @@
-﻿using UnityEngine;
+﻿/// <summary>
+/// Oscillates Game Object around its start point
+/// 
+/// Author: Albert Wohletz
+/// </summary>
+using UnityEngine;
 using System.Collections;
 
 public class Oscillate : MonoBehaviour {
-
+	public float oscillation_length = 1.0f;
+	private float start_y;
 	// Use this for initialization
 	void Start () {
-	
+		start_y = transform.position.y;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		//transform.Translate (Vector3 (0, 1, 0));
+		transform.position = new Vector3(transform.position.x,
+		                                 start_y + Mathf.Sin (Time.time) * oscillation_length,
+		                                 transform.position.z);
 	}
 }
