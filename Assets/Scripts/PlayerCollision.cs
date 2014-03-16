4f295a85cb1755fab85747a1a89a07b7ae1f70b8
Albert L Wohletz <albertlwohletz@Alberts-MacBook-Pro.local>
2014-03-16 00:25:37 -0700

    Added the collision detection scripts back in.

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,22 @@
+﻿/// <summary>
+/// Handles objects that collide with players.  Namely enemies, BnG, and powerups.
+/// 
+/// Author: Albert Wohletz
+/// </summary>
+using UnityEngine;
+using System.Collections;
+
+public class PlayerCollision : MonoBehaviour {
+	void OnCollisionEnter(Collision collision) {
+		Debug.Log ("Crash");
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
