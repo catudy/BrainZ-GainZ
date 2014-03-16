4567720e96188fab6bbf8e1f337169ab1437f849
Albert L Wohletz <albertlwohletz@Alberts-MacBook-Pro.local>
2014-03-16 13:51:44 -0700

    Added in brainz and gainz detection and counter.

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -9,13 +9,29 @@ using System.Collections;
 public class PlayerCollision : MonoBehaviour {
 	private PlayerState playerState; 
 	void OnControllerColliderHit(ControllerColliderHit collision) {
+		if (collision.gameObject.tag == "NoCollision") { // Don't care about the ground.
+			return;
+		}
+
+		// Log the collision
 		Debug.Log ("Collided with " + collision.gameObject.name);
+
+		// Filter through the collisions
 		if (collision.gameObject.name == "PowerupBlink") {
-						playerState.power_up = PowerUp.BLINK;
-						Destroy (collision.gameObject);
+			playerState.power_up = PowerUp.BLINK;
+			Destroy (collision.gameObject);
 		} else if (collision.gameObject.tag == "Deadly") { // Game over if you run into something deadly
 			GameState gameState = GameObject.Find("gameController").GetComponent<GameState>();
 			gameState.game_over = true;
+			Destroy (collision.gameObject);
+		} else if (collision.gameObject.tag == "Brainz"){
+			GameState gameState = GameObject.Find("gameController").GetComponent<GameState>();
+			gameState.brainz++;
+			Destroy (collision.gameObject);
+		} else if (collision.gameObject.tag == "Gainz"){
+			GameState gameState = GameObject.Find("gameController").GetComponent<GameState>();
+			GameState.gainz++;
+			Destroy (collision.gameObject);
 		}
 	}
 	// Use this for initialization
