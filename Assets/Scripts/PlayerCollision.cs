ce687fe52a0bd0bb1b229a3ebcb6d04b04a828f1
Albert L Wohletz <albertlwohletz@Alberts-MacBook-Pro.local>
2014-03-16 01:42:07 -0700

    Added player state value to track player attributes such as active powerup. When player runs into powerup, it now destroys powerup and gives player the powerup state.

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -7,12 +7,18 @@ using UnityEngine;
 using System.Collections;
 
 public class PlayerCollision : MonoBehaviour {
+	private PlayerState playerState; 
+
 	void OnCollisionEnter(Collision collision) {
-		Debug.Log ("Crash");
+		Debug.Log ("Collided with " + collision.gameObject.name);
+		if (collision.gameObject.name == "PowerupBlink") {
+			playerState.power_up = PowerUp.BLINK;
+			Destroy(collision.gameObject);
+		}
 	}
 	// Use this for initialization
 	void Start () {
-	
+		playerState = GetComponent<PlayerState>();
 	}
 	
 	// Update is called once per frame
---
+++ Assets/Scripts/PlayerState.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum PowerUp{
+	NONE,
+	BLINK,
+	INVISIBILITY,
+	STUN,
+	SECOND_WIND,
+	INVULNERABLE
+};
+
+public class PlayerState : MonoBehaviour {
+	public PowerUp power_up = PowerUp.NONE;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public void SetPowerUp(PowerUp set){
+		power_up = set;
+	}
+}
