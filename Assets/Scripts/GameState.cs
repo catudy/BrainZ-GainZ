5a11cfde777cd6d9ec349f9b9d3ea88ee7c3bcca
Albert Wohletz <albert@wohletz.net>
2014-03-25 16:46:41 -0700

    Camera stuff

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -10,6 +10,7 @@ public class GameState : MonoBehaviour {
 	public bool game_over = false;
 	public int brainz = 0;
 	public int gainz = 0;
+	public bool in_cutscene = true;
 
 	// Use this for initialization
 	void Start () {
@@ -18,6 +19,9 @@ public class GameState : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		if (Time.time > 30.0f) {
+			in_cutscene = false;
+		}
 		if (game_over) {
 			// Call game over here
 		}
