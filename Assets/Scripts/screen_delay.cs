543743c90c26ef27de03db79629ad1db069bdcdf
brian catudan <bcatudan@gmail.com>
2014-03-27 15:49:07 -0700

    End of our meeting merge.

--- Assets/Scripts/screen_delay.cs
+++ Assets/Scripts/screen_delay.cs
@@ -1,19 +1,44 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class screen_delay : MonoBehaviour 
 {
-	//Public variables
-	public int sceneTransTime = 1;
-	public GUITexture guiTexture;
+	public GUITexture myTexture;
+	private bool fadeOn = true;
+	private float fadeInRate = 0.05f;
+	private float fadeOutRate = 1f;
+	private float alpha;
+	private float maxFade = 0.2f;
 
-	IEnumerator Start()
+	void Update()
 	{
-		//Wait sceneTransTime before loading next scene
-		yield return new WaitForSeconds(sceneTransTime);
-
-		//Load next scene
-		Application.LoadLevel("Sushil_Test");
+		if(fadeOn)
+		{
+			FadeIn();
+		}
+		else
+		{
+			FadeOut();
+		}
 	}
 
+	void FadeIn()
+	{/*
+		while(alpha <= maxFade)
+		{
+			alpha = Mathf.Lerp (0,1,Time.deltaTime*fadeInRate);
+			myTexture.color = new Color(1,1,1,alpha);
+		}
+
+		fadeOn = false;
+		*/
+
+	}
+
+	void FadeOut()
+	{
+		myTexture.color = Color.Lerp(myTexture.color,Color.black, Time.deltaTime*fadeOutRate);
+	}
 }
+
+
