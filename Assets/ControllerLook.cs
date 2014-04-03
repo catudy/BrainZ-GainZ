af81f7ea5626d02ad696b6e52c00b49cfc71ca0b
brian catudan <bcatudan@gmail.com>
2014-04-03 13:12:27 -0700

    Some controller input. Can look with left stick, sprint with right trigger, and use ability with A. Wonky still as the camera floats in weird directions when there is no stick input and you can look down too much. Go to the player to check and uncheck which script controls you want to use.

---
+++ Assets/ControllerLook.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[AddComponentMenu("Camera-Control/Mouse Orbit")]
+
+public class ControllerLook : MonoBehaviour {
+
+	public float xSpeed = 250.0f;
+	public float ySpeed = 120.0f;
+	
+	public float yMinLimit = 0.0f;
+	public float yMaxLimit = 0.0f;
+
+	private float x = 0.0f;
+	private float y = 0.0f;
+
+	private PlayerState playerState;
+	private GameState gameState;
+
+	// Use this for initialization
+	void Start () {
+
+		playerState = GameObject.Find("Player").GetComponent<PlayerState>();
+		gameState = GameObject.Find("GameController").GetComponent<GameState>();
+	
+		Vector3 angles = transform.eulerAngles;
+		x = angles.y;
+		y = angles.x;
+
+		if(rigidbody)
+			rigidbody.freezeRotation = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void LateUpdate()
+	{
+		x += Input.GetAxis("LookX") * xSpeed * 0.02f;
+		y -= Input.GetAxis("LookY") * ySpeed * 0.02f;
+		Debug.Log (x);
+		y = ClampAngle(y,yMinLimit,yMaxLimit);
+
+		Quaternion rotation = Quaternion.Euler(y,x,0);
+
+		transform.rotation = rotation;
+
+		if(Input.GetAxis("Run") >= .2f)
+		{
+			playerState.SetRunning();
+		}
+
+		if(Input.GetButton("Ability"))
+		{
+			gameState.UseFireExtinguisher();
+		}
+	}
+
+	float ClampAngle(float angle, float min, float max)
+	{
+		if(angle<-360)
+			angle += 360;
+		if(angle >360)
+			angle -= 360;
+		return Mathf.Clamp(angle,min,max);
+	}
+
+}
