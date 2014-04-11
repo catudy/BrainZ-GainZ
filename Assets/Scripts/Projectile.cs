9cbe74ffbcfd30dd98912c65e50efadf2f6444cc
Albert Wohletz <albert@wohletz.net>
2014-04-11 15:37:29 -0700

    Fixed some names

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+
+	public Vector3 speed;
+	private Vector3 target;
+	private float start_time;
+	private Vector3 start; 
+
+	// Use this for initialization
+	void Start () {
+		speed.Set (1, 1, 1);
+		target = GameObject.Find("Player").transform.position;
+		start = transform.position;
+		start_time = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector3.Lerp (start, target, (Time.time-start_time)/(5.0f));
+	}
+}
---
+++ Assets/Scripts/ShooterAI.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShooterAI : MonoBehaviour {
+	public GameObject projectile;
+	public float cooldown = 1.0f;
+	private GameObject player;
+	private float fire_next_rocket = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Time.time > fire_next_rocket){
+			GameObject rocket = (GameObject)Instantiate(projectile, transform.position, transform.rotation);
+			Destroy (rocket, cooldown*5);
+			fire_next_rocket = Time.time + cooldown;
+		}
+	}
+}
