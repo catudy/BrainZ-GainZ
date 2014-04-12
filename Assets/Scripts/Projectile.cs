f2a25aa96aa7f7c54341ee39154c408a1a8f9ab1
Albert Wohletz <albert@wohletz.net>
2014-04-11 17:05:22 -0700

    Added new enemies

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -3,21 +3,42 @@ using System.Collections;
 
 public class Projectile : MonoBehaviour {
 
-	public Vector3 speed;
+	public ParticleSystem explosion;
 	private Vector3 target;
 	private float start_time;
 	private Vector3 start; 
+	private Vector3 velocity;
+	CharacterController cc;
 
 	// Use this for initialization
 	void Start () {
-		speed.Set (1, 1, 1);
 		target = GameObject.Find("Player").transform.position;
 		start = transform.position;
 		start_time = Time.time;
+		transform.Rotate (90, 0, 0);
+		cc = GetComponent<CharacterController> ();
+		velocity = (target - start).normalized * 5.0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.position = Vector3.Lerp (start, target, (Time.time-start_time)/(5.0f));
+		//transform.position = Vector3.Lerp (start, target, (Time.time-start_time)/(5.0f));
+		cc.Move (velocity * Time.deltaTime);
+		if(Time.time > start_time + 5.0f){
+			DestroyWithExplosion(transform.gameObject);
+		}
+	}
+
+	public void DestroyWithExplosion(GameObject obj){
+		Quaternion rotation = new Quaternion ();
+		Object o = Instantiate( explosion, obj.transform.position, rotation);
+		Destroy (obj);
+	}
+	
+	void OnControllerColliderHit(ControllerColliderHit collision) {
+		if(collision.gameObject.tag == "Deadly"){
+			DestroyWithExplosion(collision.gameObject);
+		}
+		DestroyWithExplosion(transform.gameObject);
 	}
 }
--- Assets/Scripts/ShooterAI.cs
+++ Assets/Scripts/ShooterAI.cs
@@ -3,20 +3,19 @@ using System.Collections;
 
 public class ShooterAI : MonoBehaviour {
 	public GameObject projectile;
-	public float cooldown = 1.0f;
-	private GameObject player;
+	public float cooldown = 5.0f;
 	private float fire_next_rocket = 0.0f;
 
 	// Use this for initialization
 	void Start () {
-		player = GameObject.Find("Player");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if(Time.time > fire_next_rocket){
-			GameObject rocket = (GameObject)Instantiate(projectile, transform.position, transform.rotation);
-			Destroy (rocket, cooldown*5);
+			Vector3 SpawnAt = new Vector3(transform.position.x, 0.5f, transform.position.z);
+			GameObject rocket = (GameObject)Instantiate(projectile, SpawnAt, transform.rotation);
+			Destroy (rocket, cooldown*15.0f);
 			fire_next_rocket = Time.time + cooldown;
 		}
 	}
