a02e43b5019b7c46f8e94c855dd66a68ee37c80f
Albert Wohletz <albert@wohletz.net>
2014-04-14 01:26:18 -0700

    Destroy particles from rockets after they are done.

---
+++ Assets/Destroy.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Destroy : MonoBehaviour {
+	public float time = 3.0f;
+	private float target_time;
+	// Use this for initialization
+	void Start () {
+		target_time = Time.time + time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Time.time > target_time){
+			Destroy(this.gameObject);
+		}
+	}
+}
