d22d0f21e431880fe6b50f1457260fdf663422e5
Albert Wohletz <albert@wohletz.net>
2014-04-23 00:56:30 -0700

    Fixed problems with flying zombies. Updated shooter script to use LoS for firing rather than distance.

--- Assets/Scripts/ShooterAI.cs
+++ Assets/Scripts/ShooterAI.cs
@@ -7,22 +7,20 @@ public class ShooterAI : MonoBehaviour {
 	private float fire_next_rocket = 0.0f;
 
 	//ADDED
-	public float aggro_range = 15.0f; // range in feet at which enemy will aggro
 	public float max_speed = 2.0f; // maximum speed that enemy can have.
 	public float acceleration = 1.0f; 
-	public bool aggro;// current enemy velocity.
+	public bool aggro;
 	private Vector3 target_pos;
 	private GameObject player;
 
 	// Use this for initialization
 	void Start () {
 		player = GameObject.Find("Player");
+		fire_next_rocket = Time.time + cooldown;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
-		//ADDED
 		aggro = IsAggroed (player.transform.position, transform.position);
 
 		if(Time.time > fire_next_rocket && aggro){
@@ -33,15 +31,15 @@ public class ShooterAI : MonoBehaviour {
 		}
 	}
 
-	//ADDED
 	private bool IsAggroed(Vector3 player_pos, Vector3 enemy_pos)
 	{
-		float mod_aggro_range = aggro_range;
-		
-		if (player.GetComponent<PlayerState> ().power_up == PowerUp.INVISIBILITY) 
-		{
-			mod_aggro_range = 0.0f;
+		Vector3 diff = player_pos - enemy_pos;
+		float distance = diff.magnitude;
+
+		if (Physics.Raycast (transform.position, diff, distance - 2.0f)) {
+			return false;
+		} else {
+			return true;
 		}
-		return ((player_pos - enemy_pos).magnitude < mod_aggro_range);
 	}
 }
