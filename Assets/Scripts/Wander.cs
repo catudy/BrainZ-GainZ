bf52d449d6a48fd000fd05d382fad133545ee97a
Albert Wohletz <albert@wohletz.net>
2014-04-23 00:26:22 -0700

    Updated wander script to work slightly cleaner and flexibler.

--- Assets/Scripts/Wander.cs
+++ Assets/Scripts/Wander.cs
@@ -3,10 +3,9 @@ using System.Collections;
 
 public class Wander : MonoBehaviour 
 {
-	public float wander_distance = 10.0f;
-	public float wander_speed = 10.0f;
+	public float wander_speed = 10.0f; // how fast zombies wander
+	public float wander_rate = 1.0f; // how often zombies wander
 	private float time_remaining;
-	private Vector3 target_destination;
 	private Vector3 movement_vector;
 	private CharacterController cc;
 	
@@ -23,7 +22,7 @@ public class Wander : MonoBehaviour
 		time_remaining -= Time.deltaTime;
 
 		if(time_remaining < 0.0f){
-			time_remaining = 1.0f;
+			time_remaining = wander_rate;
 			SetRandomDestination();
 		}
 		movement_vector = transform.forward;
@@ -37,33 +36,4 @@ public class Wander : MonoBehaviour
 		float angle = Random.Range (-15.0f, 15.0f);
 		transform.Rotate (yaxis, angle, Space.Self);
 	}
-
-	// Update is called once per frame
-	/*void Update () 
-	{
-		time_remaining -= Time.deltaTime;
-		movement_vector = target_destination - transform.position;
-		movement_vector.y = 0;
-
-		if(time_remaining < 0.0f || movement_vector.magnitude < wander_speed * Time.deltaTime)
-		{
-			SetRandomDestination();
-		} else 
-		{
-			Vector3 velocity = movement_vector.normalized * wander_speed;
-			velocity.y = 0;
-			Vector3 look_here = new Vector3(target_destination.x,transform.position.y, target_destination.z);
-			transform.LookAt(look_here);
-			cc.SimpleMove (velocity * Time.deltaTime);
-		}
-	}
-
-	// Sets the current_destination member variable for class.
-	private void SetRandomDestination()
-	{
-		Vector2 dest = Random.insideUnitCircle * wander_distance;
-		Vector3 current_pos = transform.position;
-		target_destination.Set (current_pos.x + dest.x, current_pos.y, current_pos.z + dest.y);
-		time_remaining = 1.0f;
-	}*/
 }
