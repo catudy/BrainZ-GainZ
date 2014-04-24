49bfa935d4cd0c860d0b0a35379f4e7ac927d386
Albert Wohletz <albert@wohletz.net>
2014-04-24 10:10:13 -0700

    Added script to despawn flames and enemies by tag.

--- Assets/Scripts/Despawner.cs
+++ Assets/Scripts/Despawner.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class Despawner : MonoBehaviour {
-
+	public GameObject[] despawn;
 	// Use this for initialization
 	void Start () {
 	
@@ -11,5 +11,11 @@ public class Despawner : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	void Despawn(){
+		foreach (GameObject o in GameObject.FindGameObjectsWithTag ("Deadly")){
+			Destroy(o);
+		}
 	}
 }
